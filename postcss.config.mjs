585d23c556074b546f0acfca3e5da0830c952859
Christopher Gu <cleegu@gmail.com>
2024-09-02 22:36:54 -0700

    failed deploy fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'postcss-import': {}, // Allows use of @import in CSS
-    'tailwindcss/nesting': {}, // Allows nesting in Tailwind CSS
     tailwindcss: {},
-    autoprefixer: {}, // Adds vendor prefixes to CSS
   },
 };
 
