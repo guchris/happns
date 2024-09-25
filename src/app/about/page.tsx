26fc2e26863d86f7eb30b3bc3ac712c8801e213d
Christopher Gu <cleegu@gmail.com>
2024-09-25 13:13:31 -0700

    1) replaced escaped chars

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,13 +16,13 @@ export default function AboutPage() {
                     Happns was created by Christopher Gu in Seattle out of a desire to solve a personal problem: he found it difficult to discover what was happening in his city. As someone who loves exploring local events and connecting with the community, Christopher was frustrated by the lack of a centralized platform that aggregated all the interesting happenings around town.
                 </p>
                 <p className="text-sm">
-                    Inspiration struck one day while he was walking through the neighborhoods of Seattle, glancing at electricity poles plastered with flyers for events. These poles, covered in layers of paper and staples, were the closest thing he could find to a comprehensive event listing. But it wasn’t practical. There had to be a better way to bring together all the city’s events in one place.
+                    Inspiration struck one day while he was walking through the neighborhoods of Seattle, glancing at electricity poles plastered with flyers for events. These poles, covered in layers of paper and staples, were the closest thing he could find to a comprehensive event listing. But it wasn&apos;t practical. There had to be a better way to bring together all the city&apos;s events in one place.
                 </p>
                 <p className="text-sm">
                     From this experience, happns was born. Christopher envisioned a platform where curated events could be easily found, shared, and experienced. His goal was to create a digital space that captures the vibrant pulse of the city, showcasing everything from community meetups to music festivals and art shows. Whether you're looking for a fun night out, a new hobby, or a way to connect with like-minded people, happns is here to help you discover the best of your city.
                 </p>
                 <p className="text-sm">
-                    Today, happns is filled with handpicked events, making it easier than ever for Seattleites to find out what’s happening around them. Christopher hopes to expand happns to more cities, helping communities everywhere stay connected and engaged.
+                    Today, happns is filled with handpicked events, making it easier than ever for Seattleites to find out what&apos;s happening around them. Christopher hopes to expand happns to more cities, helping communities everywhere stay connected and engaged.
                 </p>
             </div>
         </div>
