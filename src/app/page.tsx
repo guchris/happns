ba573da9a9e629ef3a2a799777dcf1742855e20c
Christopher Gu <cleegu@gmail.com>
2024-10-12 21:16:33 -0600

    1) set up firebase admin, 2) get user and user default city server side via token management, 3) kept geolocation in client side components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,178 +1,203 @@
 // Next Imports
 import { Metadata } from "next"
+import { cookies } from "next/headers"
 
 // App Imports
 import { TopBar } from "@/components/top-bar"
 import { Footer } from "@/components/footer"
-import { CitySelector } from "@/components/city-selector"
 import { EventCarousel } from "@/components/event-carousel"
 import { CityGrid } from "@/components/city-grid"
 import JoinCard from "@/components/join-card"
+import CitySelector from "@/components/city-selector"
 import EventGridDynamic from "@/components/event-grid-dynamic"
-import { CarouselEvent } from "@/components/types"
+import { City, CarouselEvent } from "@/components/types"
 import { getTotalUpcomingEvents } from "@/lib/eventUtils"
 
 // Firebase Imports
-import { db } from "@/lib/firebase"
-import { collection, doc, getDoc, getDocs } from "firebase/firestore"
+import { auth, db } from "@/lib/firebaseAdmin"
 
 // Shadcn Imports
 import { Separator } from "@/components/ui/separator"
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
+import { Card, CardTitle } from "@/components/ui/card"
 import { Music, PartyPopper, Theater, Globe, Dumbbell, Users, Gamepad, Film } from "lucide-react"
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic'
 
 // Generate page metadata
 export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "happns | events in your city",
-    description: "discover curated events happening in your city with happns",
-    openGraph: {
-      title: "happns | events in your city",
-      description: "discover curated events happening in your city with happns",
-      images: ["https://ithappns.com/logo.png"],
-      url: "https://ithappns.com",
-      type: "website",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "happns | events in your city",
-      description: "discover curated events happening in your city with happns",
-      images: ["https://ithappns.com/logo.png"],
-    },
-  };
+	return {
+		title: "happns | events in your city",
+		description: "discover curated events happening in your city with happns",
+		openGraph: {
+			title: "happns | events in your city",
+			description: "discover curated events happening in your city with happns",
+			images: ["https://ithappns.com/logo.png"],
+			url: "https://ithappns.com",
+			type: "website",
+		},
+		twitter: {
+			card: "summary_large_image",
+			title: "happns | events in your city",
+			description: "discover curated events happening in your city with happns",
+			images: ["https://ithappns.com/logo.png"],
+		},
+	};
 }
 
 // Fetch cities and total upcoming events
 async function fetchCities() {
-  const citiesRef = collection(db, "cities");
-  const citySnapshot = await getDocs(citiesRef);
-  
-  // Use Promise.all to fetch event counts for all cities in parallel
-  const cities = await Promise.all(
-    citySnapshot.docs.map(async (doc) => {
-      const cityData = doc.data();
-      const upcomingEventCount = await getTotalUpcomingEvents(cityData.slug);
-      return {
-        id: doc.id,
-        name: cityData.name,
-        slug: cityData.slug,
-        lat: cityData.lat,
-        lon: cityData.lon,
-        slogan: cityData.slogan,
-        description: cityData.description,
-        upcomingEventCount,
-      };
-    })
-  );
+	const citiesRef = db.collection("cities");
+  	const citySnapshot = await citiesRef.get();
 
-  return cities;
+	// For each city, fetch event counts in parallel
+	const cities = await Promise.all(
+		citySnapshot.docs.map(async (doc) => {
+			const cityData = doc.data();
+			const upcomingEventCount = await getTotalUpcomingEvents(cityData.slug);
+			return {
+				id: doc.id,
+				name: cityData.name,
+				slug: cityData.slug,
+				lat: cityData.lat,
+				lon: cityData.lon,
+				slogan: cityData.slogan,
+				description: cityData.description,
+				upcomingEventCount,
+			};
+		})
+	);
+
+	return cities;
 }
 
 // Fetch carousel event data
 async function fetchCarouselEvents() {
-  const carouselRef = collection(db, "carousel");
-  const carouselSnapshot = await getDocs(carouselRef);
+	const carouselRef = db.collection("carousel");
+  	const carouselSnapshot = await carouselRef.get();
 
-  // For each carousel item, fetch the corresponding event details from the "events" collection
-  const carouselEvents = await Promise.all(
-    carouselSnapshot.docs.map(async (carouselDoc) => {
-      const eventId = carouselDoc.id;
+	// For each carousel item, fetch the corresponding event details from the "events" collection in parallel
+	const carouselEvents = await Promise.all(
+		carouselSnapshot.docs.map(async (carouselDoc) => {
+			const eventId = carouselDoc.id;
+			const eventDocRef = db.collection("events").doc(eventId);
+			const eventDoc = await eventDocRef.get();
 
-      const eventDocRef = doc(db, "events", eventId);
-      const eventDoc = await getDoc(eventDocRef);
+			if (eventDoc.exists) {
+				const eventData = eventDoc.data();
+				return {
+					uid: eventId,
+					image: eventData!.image,
+				};
+			} else {
+				return null;
+			}
+		})
+	);
 
-      if (eventDoc.exists()) {
-        const eventData = eventDoc.data();
-        return {
-          uid: eventId,
-          image: eventData.image,
-        };
-      } else {
-        return null;
-      }
-    })
-  );
+	// Filter out any null values (in case an event was not found)
+	return carouselEvents.filter((event): event is CarouselEvent => event !== null);
+}
 
-  // Filter out any null values (in case an event was not found)
-  return carouselEvents.filter((event): event is CarouselEvent => event !== null);
+// Function to get the user's default city from Firestore
+async function getUserCity(userId: string): Promise<string | null> {
+	const userDocRef = db.collection("users").doc(userId);
+	const userDoc = await userDocRef.get();
+
+	if (userDoc.exists) {
+		const userData = userDoc.data();
+		return userData?.selectedCity || null;
+	}
+	return null;
 }
 
 export default async function Home() {
+	const token = cookies().get('auth_token')?.value;
+  	let userCity = null;
 
-  const cities = await fetchCities();
-  const carouselEvents = await fetchCarouselEvents();
+	if (token) {
+		try {
+			const decodedToken = await auth.verifyIdToken(token);
+			const userId = decodedToken.uid;
+			userCity = await getUserCity(userId);
+		} catch (error) {
+			console.error("Error verifying token or retrieving user city:", error);
+		}
+	}
 
-  return (
-    <div>
-      {/* Intro Animation */}
-      <div className="intro-animation">
-          <h1>happns/</h1>
-      </div>
-      <div className="flex flex-col min-h-screen">
-        <TopBar title="happns" />
-        <Separator />
+	// Determine the city to use
+	const cities = await fetchCities();
+	const carouselEvents = await fetchCarouselEvents();
+	const initialCity = userCity || "seattle";
 
-        <div className="flex-1 overflow-y-auto">
+	return (
+		<div>
+			{/* Intro Animation */}
+			<div className="intro-animation">
+				<h1>happns/</h1>
+			</div>
+			<div className="flex flex-col min-h-screen">
+			<TopBar title="happns" />
+			<Separator />
 
-          {/* Hero Section */}
-          <div className="flex flex-col max-w-[880px] md:max-w-[700px] lg:max-w-[880px] mx-auto py-16 p-4 space-y-8 items-center lg:flex-row lg:space-x-12">
-              
-              {/* Left Section: Slogan, City Selector */}
-              <div className="lg:w-1/2 space-y-4">
-                  <h2 className="text-3xl font-bold">discover curated events happning in your city</h2>
-                  <CitySelector cities={cities} />
-              </div>
+			<div className="flex-1 overflow-y-auto">
 
-              {/* Right Section: Event Photo Carousel */}
-              <EventCarousel carouselEvents={carouselEvents} />
+				{/* Hero Section */}
+				<div className="flex flex-col max-w-[880px] md:max-w-[700px] lg:max-w-[880px] mx-auto py-16 p-4 space-y-8 items-center lg:flex-row lg:space-x-12">
+					
+					{/* Left Section: Slogan, City Selector */}
+					<div className="lg:w-1/2 space-y-4">
+						<h2 className="text-3xl font-bold">discover curated events happning in your city</h2>
+						<CitySelector cities={cities} initialCity={initialCity} />
+					</div>
 
-          </div>
-          
-          <Separator />
+					{/* Right Section: Event Photo Carousel */}
+					<EventCarousel carouselEvents={carouselEvents} />
 
-          <div className="py-12 space-y-8">
+				</div>
+				
+				<Separator />
 
-            {/* Events Grid */}
-            <EventGridDynamic cities={cities} />
+				<div className="py-12 space-y-8">
 
-            {/* City Grid */}
-            <CityGrid cities={cities} />
+				{/* Events Grid */}
+				<EventGridDynamic cities={cities} initialCity={initialCity} />
 
-            {/* Join Card */}
-            <JoinCard />
+				{/* City Grid */}
+				<CityGrid cities={cities} />
 
-            {/* Categories Section */}
-            <div className="flex-1 max-w-[880px] md:max-w-[700px] lg:max-w-[880px] mx-auto p-4 space-y-4">
-              <h3 className="text-xl font-semibold">explore top categories</h3>
-              <div className="grid grid-cols-4 lg:grid-cols-6 gap-4">
-                  {[
-                      { name: "music", icon: <Music /> },
-                      { name: "nightlife", icon: <PartyPopper /> },
-                      { name: "arts", icon: <Theater /> },
-                      { name: "culture", icon: <Globe /> },
-                      { name: "fitness", icon: <Dumbbell /> },
-                      { name: "family", icon: <Users /> },
-                      { name: "gaming", icon: <Gamepad /> },
-                      { name: "film", icon: <Film /> }
-                  ].map((category) => (
-                      <div key={category.name} className="pointer-events-none opacity-50">
-                          <Card key={category.name} className="flex flex-col items-center justify-center p-4 h-24 space-y-1">
-                              <div className="flex items-center justify-center w-10 h-10 text-muted-foreground">{category.icon}</div>
-                              <CardTitle className="line-clamp-1 text-center text-base font-medium">{category.name}</CardTitle>
-                          </Card>
-                      </div>
-                  ))}
-              </div>
-            </div>
+				{/* Join Card */}
+				<JoinCard />
 
-          </div>
-        </div>
+				{/* Categories Section */}
+				<div className="flex-1 max-w-[880px] md:max-w-[700px] lg:max-w-[880px] mx-auto p-4 space-y-4">
+					<h3 className="text-xl font-semibold">explore top categories</h3>
+					<div className="grid grid-cols-4 lg:grid-cols-6 gap-4">
+						{[
+							{ name: "music", icon: <Music /> },
+							{ name: "nightlife", icon: <PartyPopper /> },
+							{ name: "arts", icon: <Theater /> },
+							{ name: "culture", icon: <Globe /> },
+							{ name: "fitness", icon: <Dumbbell /> },
+							{ name: "family", icon: <Users /> },
+							{ name: "gaming", icon: <Gamepad /> },
+							{ name: "film", icon: <Film /> }
+						].map((category) => (
+							<div key={category.name} className="pointer-events-none opacity-50">
+								<Card className="flex flex-col items-center justify-center p-4 h-24 space-y-1">
+									<div className="flex items-center justify-center w-10 h-10 text-muted-foreground">{category.icon}</div>
+									<CardTitle className="line-clamp-1 text-center text-base font-medium">{category.name}</CardTitle>
+								</Card>
+							</div>
+						))}
+					</div>
+				</div>
 
-        <Footer />
+				</div>
+			</div>
 
-      </div>
-    </div>
-  );
+			<Footer />
+
+			</div>
+		</div>
+	);
 }
--- src/components/city-selector.tsx
+++ src/components/city-selector.tsx
@@ -1,128 +1,81 @@
 "use client"
 
-// Next Imports
-import { useEffect, useState } from "react"
+// Next and React Imports
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 // App Imports
 import { City } from "@/components/types"
-import { useAuth } from "@/context/AuthContext"
 import { calculateDistance } from "@/lib/geoUtils"
 
-// Firebase Imports
-import { db } from "@/lib/firebase"
-import { doc, getDoc } from "firebase/firestore"
-
 // Shadcn Imports
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Button } from "@/components/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 interface CitySelectorProps {
     cities: City[];
+    initialCity: string;
 }
 
-export function CitySelector({ cities }: CitySelectorProps) {
-    const { user } = useAuth();
-    const [selectedCity, setSelectedCity] = useState<string | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
+const CitySelector = ({ cities, initialCity }: CitySelectorProps) => {
+    const [selectedCity, setSelectedCity] = useState<string>("");
 
-    // Function to find the closest city based on the user's location
-    const findClosestCity = (userLat: number, userLon: number) => {
-        let closestCity = cities[0];
-        let minDistance = calculateDistance(userLat, userLon, cities[0].lat, cities[0].lon);
-
-        cities.forEach((city) => {
-        const distance = calculateDistance(userLat, userLon, city.lat, city.lon);
-        if (distance < minDistance) {
-            closestCity = city;
-            minDistance = distance;
-        }
-        });
-
-        return closestCity.slug; // Return the slug of the closest city
-    };
-
-    // Set city selection
     useEffect(() => {
-        // Load default city from Firestore if available
-        const loadUserCity = async () => {
-            if (user) {
-                const userDocRef = doc(db, "users", user.uid);
-                const userDoc = await getDoc(userDocRef);
-
-                if (userDoc.exists()) {
-                    const userData = userDoc.data();
-                    if (userData.selectedCity && userData.selectedCity !== "") {
-                        setSelectedCity(userData.selectedCity);
-                        setIsLoading(false);
-                        return; // Exit the function if default city is set
-                    }
+        if (initialCity) {
+            // Use the user's default city if it’s set
+            setSelectedCity(initialCity);
+        } else if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const userLat = position.coords.latitude;
+                    const userLon = position.coords.longitude;
+                    let closestCity = cities[0];
+                    let minDistance = calculateDistance(userLat, userLon, cities[0].lat, cities[0].lon);
+    
+                    cities.forEach((city) => {
+                        const distance = calculateDistance(userLat, userLon, city.lat, city.lon);
+                        if (distance < minDistance) {
+                            closestCity = city;
+                            minDistance = distance;
+                        }
+                    });
+                    setSelectedCity(closestCity.slug);
+                },
+                () => {
+                    // If geolocation fails, use a fallback city if needed
+                    setSelectedCity("seattle");
                 }
-            }
-
-            // If no default city, fallback to geolocation
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                    (position) => {
-                        const userLat = position.coords.latitude;
-                        const userLon = position.coords.longitude;
-                        const closestCitySlug = findClosestCity(userLat, userLon);
-                        setSelectedCity(closestCitySlug);
-                        setIsLoading(false);
-                    },
-                    (error) => {
-                        console.error("Error getting user location:", error);
-                        setSelectedCity("seattle"); // Fallback city if geolocation fails
-                        setIsLoading(false);
-                    }
-                );
-            } else {
-                setSelectedCity("seattle"); // Fallback city if geolocation is unavailable
-                setIsLoading(false);
-            }
-        };
-
-        loadUserCity();
-    }, [user, cities]);
+            );
+        } else {
+            setSelectedCity("seattle");
+        }
+    }, [cities, initialCity]);
 
     return (
         <div className="flex items-center space-x-2">
-            {isLoading ? (
-                // Temporary loading placeholder
-                <>  
-                    <Select>
-                        <SelectTrigger disabled>
-                            <SelectValue placeholder="Loading location..." />
-                        </SelectTrigger>
-                    </Select>
-                    <Button disabled>explore</Button>
-                </>
-            ) : selectedCity ? (
-                <>
-                    <Select value={selectedCity ?? undefined} onValueChange={(value) => setSelectedCity(value)}>
-                        <SelectTrigger>
-                            <SelectValue>{cities.find((city) => city.slug === selectedCity)?.name || "Select your city"}</SelectValue>
-                        </SelectTrigger>
-                        <SelectContent>
-                            {cities.map((city) => (
-                                <SelectItem key={city.slug} value={city.slug}>
-                                    {city.name}
-                                </SelectItem>
-                            ))}
-                        </SelectContent>
-                    </Select>
+            <Select value={selectedCity} onValueChange={(value) => setSelectedCity(value)}>
+                <SelectTrigger>
+                    <SelectValue>
+                        {selectedCity === "" 
+                            ? "loading city..." 
+                            : cities.find((city) => city.slug === selectedCity)?.name || "select your city"}
+                    </SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                    {cities.map((city) => (
+                        <SelectItem key={city.slug} value={city.slug}>
+                            {city.name}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
 
-                    {/* Explore Button */}
-                    <Link href={`/${selectedCity}/explore`}>
-                        <Button>explore</Button>
-                    </Link>
-                </>
-            ) : (
-                // Fallback UI in case `selectedCity` fails to set
-                <div>
-                    <p>Error: Could not set city</p>
-                </div>
-            )}
+            {/* Explore Button */}
+            <Link href={`/${selectedCity}/explore`}>
+                <Button>explore</Button>
+            </Link>
         </div>
     )
 }
+
+export default CitySelector;
