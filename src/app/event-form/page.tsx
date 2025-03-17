9d36448fae304b54c9c530efeca822bfea558c9f
Christopher Gu <cleegu@gmail.com>
2025-03-17 14:45:13 -0700

    1) removed useSearchParams from event-form

--- src/app/event-form/page.tsx
+++ src/app/event-form/page.tsx
@@ -3,14 +3,10 @@
 // Next and React Imports
 import { Suspense } from "react"
 import dynamic from "next/dynamic"
-import { useSearchParams } from "next/navigation"
 
 const EventForm = dynamic(() => import('@/components/event-form'), { ssr: false });
 
 export default function EventFormPage() {
-    const searchParams = useSearchParams();
-    const eventId = searchParams?.get("id");
-    const mode = eventId ? "edit" : "create";
 
     return (
         <Suspense>
